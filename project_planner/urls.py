17ea0193d2054133dd50e75700a944707b780cf9
kb787 <karanbhanu787@gmail.com>
2025-04-30 22:07:56 +0530

    changedMore

--- project_planner/urls.py
+++ project_planner/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/api/users',include('users.urls')),
-    path('/api/boards',include('boards.urls')),
-    path('/api/teams',include('teams.urls')),
+    path('api/users/',include('users.urls')),
+    path('api/boards/',include('boards.urls')),
+    path('api/teams/',include('teams.urls')),
 ]    
